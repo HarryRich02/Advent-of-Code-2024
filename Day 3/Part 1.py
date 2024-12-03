48e2eae6fbaa5328754af8465f29d2c6978aea87
HarryRich02 <harryrichards2202@gmail.com>
2024-12-03 13:49:42 +0000

    Day 3 (2 lines)

--- Day 3/Part 1.py
+++ Day 3/Part 1.py
@@ -1,3 +1,4 @@
 import re
 with open("input.txt", "r") as f:
-    print(sum(x for xs in [list(map(lambda x: int(x[0])*int(x[1]), x)) for x in [re.findall("mul\((\d+),(\d+)\)", line) for line in f]] for x in xs))
+    data = sum(x for xs in [list(map(lambda x: int(x[0])*int(x[1]), x)) for x in [re.findall("mul\((\d+),(\d+)\)", line) for line in f]] for x in xs)
+print(data)
