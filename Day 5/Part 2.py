fe8f68016de930115d6ca8acd23f8982386cea6e
HarryRich02 <klvr36@durham.ac.uk>
2024-12-05 14:52:20 +0000

    Day 5 Part 2
    Complete

--- Day 5/Part 2.py
+++ Day 5/Part 2.py
@@ -1,6 +1,6 @@
 import re
 
-with open("test.txt", "r") as f:
+with open("input.txt", "r") as f:
     data = [lines.strip("\n").split("|") for lines in f]
 rules = data[:data.index([''])]
 pages = [x for xs in data[data.index([''])+1:] for x in xs]
@@ -12,32 +12,32 @@ for i in range(len(pages)):
     if re.search(regex, pages[i]):
         wrongPages.append(pages[i])
 
-values = set([x for xs in [x.split(",") for x in pages] for x in xs])
-rules = [[x[0] for x in rules], [x[1] for x in rules]]
-
-sorted = []
-while len(sorted) != len(values):
-    try:
-        nextPage = list(values - set(rules[1]))[0]
-    except:
-        break
-    sorted.append(nextPage)
-    while nextPage in rules[0]:
-        index = rules[0].index(nextPage)
-        rules[0].pop(index)
-        rules[1].pop(index)
-    values.remove(nextPage)
-
-total = 0
 wrongPages = [x.split(",") for x in wrongPages]
+total = 0
 for i in range(len(wrongPages)):
-    sortedPage = []
-    for j in range(len(sorted)):
-        if sorted[j] in wrongPages[i]:
-            sortedPage.append(sorted[j])
-    try:
-        total += int(sortedPage[len(sortedPage)//2])
-    except:
-        pass
+    rules = data[:data.index([''])]
+
+    j = 0
+    while j < len(rules):
+        if not ((rules[j][0] in wrongPages[i]) and (rules[j][1] in wrongPages[i])):
+            rules.pop(j)
+        else:
+            j += 1
+    
+    rules = [[x[0] for x in rules], [x[1] for x in rules]]
+    sorted = []
+    values = set(wrongPages[i])
+    while len(sorted) != len(wrongPages[i]):
+        try:
+            nextPage = list(values - set(rules[1]))[0]
+        except:
+            break
+        sorted.append(nextPage)
+        while nextPage in rules[0]:
+            index = rules[0].index(nextPage)
+            rules[0].pop(index)
+            rules[1].pop(index)
+        values.remove(nextPage)
+    total += int(sorted[len(sorted)//2])
 
 print(total)
